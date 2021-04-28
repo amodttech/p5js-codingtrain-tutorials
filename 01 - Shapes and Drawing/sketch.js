d4557316b277390b2a1a759a0e5eb7368284afae
Aaron Amodt <70347744+amodttech@users.noreply.github.com>
2021-04-28 19:04:17 -0400

    shapes drawn

---
+++ 01 - Shapes and Drawing/sketch.js
@@ -0,0 +1,29 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  //  grayscale: background(51)
+  //  rbg: background(255, 204, 0)
+  //  named color: background('red')
+  //  hexadecimal color: background('#222222')
+
+
+
+  rect(50,50,25,75) 
+  //  rect(x-origin, y-origin, width, height)
+
+
+
+  line(10,330,100,200)
+  // line(x1, y1, x2, y2)
+  // line(x1, y1, z1, x2, y2, z2)
+
+
+
+
+
+}
+
+
