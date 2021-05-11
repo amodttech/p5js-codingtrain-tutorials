981d68ba82db34ec4e9e0eaf889b75611960f44d
Aaron <aaron@amodttech.com>
2021-05-11 19:30:11 -0400

    new functions

--- 03-Variables in p5js/sketch.js
+++ 03-Variables in p5js/sketch.js
@@ -4,14 +4,22 @@
 // setup is run ONCE per program, at the begining.  which is why it lives at the top
 function setup() {
   createCanvas(400,300)
+  background(0)  // by moving backg4round into setup, it's only drawn once.  so now the circle in the draw function acts as a paintbrush
+
 }
 
 //  draw is a forever loop, but it still run in order.  
 
 function draw() {
-  background(0)
+  
 
   noStroke()  /// the color changing functions must be defined before the shape function
   fill(255)
-  ellipse(200,150,75,75)
+
+  // mouseX and mouseY are built in variables that are the cursor position relative to the canvas 0,0
+  circle(mouseX,mouseY,20)
+}
+
+function mousePressed() {  // another built in function, mouse click event listener
+  background(0)   // if background is called in a mousePressed, then it will redrew the background, effectively erasing the canvas
 }
