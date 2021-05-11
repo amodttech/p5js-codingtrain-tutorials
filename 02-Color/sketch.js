5751529b91c3702543de64eda28de2a46d1b2e8f
Aaron <aaron@amodttech.com>
2021-05-11 14:35:40 -0400

    colors

---
+++ 02-Color/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400,300)
+}
+
+function draw() {
+  background(220,0,220)
+
+  
+  rectMode(CENTER)
+  
+  fill(0,0,155)
+  stroke(0,0,0)
+  rect(200,150,150,150)
+}
